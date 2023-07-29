a2a068866ee8e8924cecb7f5e7190934071909e1
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-07-29 18:58:11 -0300

    sav

--- listacompras/src/components/Pessoa.js
+++ listacompras/src/components/Pessoa.js
@@ -1,6 +1,8 @@
+import styles from './Pessoa.module.css'
+
 function Pessoa(props){
   return(
-    <div>
+    <div className={styles.info}>
       <h1>Nome: {props.nome}</h1>
       <h2>Idade: {props.idade}</h2>
       <h2>Profissão: {props.profissao}</h2>
